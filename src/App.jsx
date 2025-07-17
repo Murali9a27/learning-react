33fb28b99f08b5e61ae8ce25e8d43af2e5a5c57e
Murali9a27 <murali9a27@gmail.com>
2025-07-17 23:39:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Murali 👋</h1>
+      <p>This is your first React app!</p>
+    </div>
+  )
+}
+
+export default App
