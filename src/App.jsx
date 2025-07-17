d3c95ebe14e27cd7c752d7f189b651a54bbf7050
Murali27a <murali27a@gmail.com>
2025-07-18 00:15:32 +0530

    learn creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
+      <Header></Header>
+      <Content style={{color:'#666', backgroundColor:'#edefef'}}></Content>
+      <Footer></Footer>
       <h1>Hello, Murali 👋</h1>
       <p>This is your first React app!</p>
     </div>
---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Content(props) {
+  return (
+    <main style={props.style}>
+      <p>This is the content area where main info goes.</p>
+    </main>
+  )
+}
+
+export default Content
